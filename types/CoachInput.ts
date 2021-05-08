1eea8bf6b8b80fcb3bd4ce2742d1655162c3cac2
Reece Kidd <rkidd@Reeces-MacBook-Pro.local>
2021-05-08 08:32:48 +0100

    refactor: updated schema

--- types/CoachInput.ts
+++ types/CoachInput.ts
@@ -1,13 +1,9 @@
 import { InputType, Field } from 'type-graphql'
 
-import { ObjectId } from 'mongodb'
 import { Coach } from '../entity/Coach'
 
 @InputType()
 export class CoachInput implements Partial<Coach> {
-  @Field({ nullable: true })
-  id?: ObjectId
-
   @Field()
   name: string
 
