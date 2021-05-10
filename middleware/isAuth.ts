139404d3ddbe2e1d4f334fd991f05be472c3e914
Reece Kidd <rkidd@Reeces-MacBook-Pro.local>
2021-05-10 09:23:25 +0100

    refactor: Added user

---
+++ middleware/isAuth.ts
@@ -0,0 +1,20 @@
+import { MiddlewareFn } from 'type-graphql'
+import { MyContext } from '../types/MyContext'
+import jwt from 'jsonwebtoken'
+
+const APP_SECRET = process.env.SESSION_SECRET || 'aslkdfjoiq12312'
+
+export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
+  const authorization = context.req.headers['authorization']
+  if (!authorization) {
+    throw new Error('Missing authorization')
+  }
+  try {
+    const token = authorization?.replace('Bearer ', '')
+    const user = jwt.verify(token, APP_SECRET) as any
+    context.res.locals.userId = user.id
+    return next()
+  } catch (err) {
+    throw new Error(err.message)
+  }
+}
--- schema/index.ts
+++ schema/index.ts
@@ -6,10 +6,11 @@ import path from 'path'
 import { ObjectIdScalar } from './object-id.scalar'
 import { TypegooseMiddleware } from '../middleware/typegoose'
 import { CoachResolver } from '../resolver/Coach'
+import { UserResolver } from '../resolver/User'
 
 export default async function createSchema(): Promise<GraphQLSchema> {
   const schema = await buildSchema({
-    resolvers: [CoachResolver],
+    resolvers: [CoachResolver, UserResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
     globalMiddlewares: [TypegooseMiddleware],
     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
