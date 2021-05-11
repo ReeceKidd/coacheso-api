1d38682d402239ae889c215c2ae76b658e0fe3e7
Reece Kidd <rkidd@Reeces-MacBook-Pro.local>
2021-05-11 07:54:51 +0100

    chore: updated getServiceConfig unit tests

--- getServiceConfig.spec.ts
+++ getServiceConfig.spec.ts
@@ -24,6 +24,13 @@ describe('getServiceConfig', () => {
     }
   })
 
+  test('that NODE_ENV is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.NODE_ENV).toBeDefined()
+  })
+
   test('that correct error is thrown when PORT is not provided', () => {
     expect.assertions(1)
     const environment = {
@@ -36,6 +43,13 @@ describe('getServiceConfig', () => {
     } catch (err) {
       expect(err.message).toEqual('PORT is not provided.')
     }
+  })
+
+  test('that PORT is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.PORT).toBeDefined()
   })
 
   test('that correct error is thrown when DATABASE_URI is not provided', () => {
@@ -52,6 +66,13 @@ describe('getServiceConfig', () => {
     }
   })
 
+  test('that DATABASE_URI is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.DATABASE_URI).toBeDefined()
+  })
+
   test('that correct error is thrown when AUTH0_CLIENT_ID is not provided', () => {
     expect.assertions(1)
     const environment = {
@@ -66,6 +87,13 @@ describe('getServiceConfig', () => {
     }
   })
 
+  test('that AUTH0_CLIENT_ID is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.AUTH0_CLIENT_ID).toBeDefined()
+  })
+
   test('that correct error is thrown when AUTH0_DOMAIN is not provided', () => {
     expect.assertions(1)
     const environment = {
@@ -78,5 +106,33 @@ describe('getServiceConfig', () => {
     } catch (err) {
       expect(err.message).toEqual('AUTH0_DOMAIN is not provided.')
     }
+  })
+
+  test('that AUTH0_DOMAIN is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.AUTH0_DOMAIN).toBeDefined()
+  })
+
+  test('that correct error is thrown when AUTH0_CLIENT_SECRET is not provided', () => {
+    expect.assertions(1)
+    const environment = {
+      ...environmentMock,
+      AUTH0_CLIENT_SECRET: undefined,
+    }
+
+    try {
+      getServiceConfig(environment)
+    } catch (err) {
+      expect(err.message).toEqual('AUTH0_CLIENT_SECRET is not provided.')
+    }
+  })
+
+  test('that AUTH0_CLIENT_SECRET is returned', () => {
+    expect.assertions(1)
+
+    const environment = getServiceConfig()
+    expect(environment.AUTH0_CLIENT_SECRET).toBeDefined()
   })
 })
