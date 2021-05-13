74122cc589874bcb6b52fcb94b48df82b6bf0700
Reece Kidd <rkidd@Reeces-MacBook-Pro.local>
2021-05-13 08:19:06 +0100

    refactor: successfully verified jwt on server

--- getServiceConfig.ts
+++ getServiceConfig.ts
@@ -5,9 +5,6 @@ export interface AppConfigHttp {
   NODE_ENV: string
   PORT: string
   DATABASE_URI: string
-  AUTH0_CLIENT_ID: string
-  AUTH0_DOMAIN: string
-  AUTH0_CLIENT_SECRET: string
 }
 
 export type AppConfig = AppConfigHttp
@@ -15,14 +12,7 @@ export type AppConfig = AppConfigHttp
 export const getServiceConfig = (
   environment: NodeJS.ProcessEnv = process.env
 ): AppConfig => {
-  const {
-    NODE_ENV,
-    PORT,
-    DATABASE_URI,
-    AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN,
-    AUTH0_CLIENT_SECRET,
-  } = environment
+  const { NODE_ENV, PORT, DATABASE_URI } = environment
 
   if (!NODE_ENV) throw new Error('NODE_ENV is not provided.')
 
@@ -30,19 +20,9 @@ export const getServiceConfig = (
 
   if (!DATABASE_URI) throw new Error('DATABASE_URI is not provided.')
 
-  if (!AUTH0_CLIENT_ID) throw new Error('AUTH0_CLIENT_ID is not provided.')
-
-  if (!AUTH0_DOMAIN) throw new Error('AUTH0_DOMAIN is not provided.')
-
-  if (!AUTH0_CLIENT_SECRET)
-    throw new Error('AUTH0_CLIENT_SECRET is not provided.')
-
   return {
     NODE_ENV,
     PORT,
     DATABASE_URI,
-    AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN,
-    AUTH0_CLIENT_SECRET,
   } as AppConfigHttp
 }
