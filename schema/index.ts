f1263b3623e85db3dee51102700849fe49596c6e
Reece Kidd <reecekidd95@gmail.com>
2021-05-30 16:03:58 +0100

    refactor: can search for skills

--- schema/index.ts
+++ schema/index.ts
@@ -7,10 +7,11 @@ import { ObjectIdScalar } from './object-id.scalar'
 import { TypegooseMiddleware } from '../graphql-middleware/typegoose'
 import { CoachResolver } from '../resolver/Coach'
 import { UserResolver } from '../resolver/User'
+import { SkillResolver } from '../resolver/Skill'
 
 export default async function createSchema(): Promise<GraphQLSchema> {
   const schema = await buildSchema({
-    resolvers: [CoachResolver, UserResolver],
+    resolvers: [CoachResolver, UserResolver, SkillResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
     globalMiddlewares: [TypegooseMiddleware],
     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
