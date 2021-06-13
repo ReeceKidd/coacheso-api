a7094c7dffa5cf3eea97695cb2f07bb887c0803d
Reece Kidd <reecekidd95@gmail.com>
2021-06-13 16:56:42 +0100

    refactor: created first version of requests

---
+++ resolver/Request.ts
@@ -0,0 +1,45 @@
+import {
+  Resolver,
+  Query,
+  Arg,
+  Mutation,
+  UseMiddleware,
+  Ctx,
+} from 'type-graphql'
+
+import { isAuth } from '../graphql-middleware/isAuth'
+import { Request, RequestModel, RequestType } from '../entity/Request'
+import { MyContext } from '../types/MyContext'
+import { RequestInput } from '../types/RequestInput'
+
+@Resolver(() => Request)
+export class RequestResolver {
+  @Query(() => [Request])
+  @UseMiddleware(isAuth)
+  async coachingRequests(
+    @Ctx()
+    ctx: MyContext
+  ): Promise<Request[]> {
+    const currentRequests = await RequestModel.find({
+      coachId: ctx.res.locals.user._id,
+      type: RequestType.coaching,
+    })
+
+    return currentRequests
+  }
+
+  @Mutation(() => Request)
+  @UseMiddleware(isAuth)
+  async sendRequest(
+    @Ctx()
+    ctx: MyContext,
+    @Arg('input') input: RequestInput
+  ): Promise<Request> {
+    const request = new RequestModel({
+      ...input,
+      userId: ctx.res.locals.user._id,
+    })
+    await request.save()
+    return request
+  }
+}
--- schema/index.ts
+++ schema/index.ts
@@ -8,10 +8,11 @@ import { TypegooseMiddleware } from '../graphql-middleware/typegoose'
 import { CoachResolver } from '../resolver/Coach'
 import { UserResolver } from '../resolver/User'
 import { SkillResolver } from '../resolver/Skill'
+import { RequestResolver } from '../resolver/Request'
 
 export default async function createSchema(): Promise<GraphQLSchema> {
   const schema = await buildSchema({
-    resolvers: [CoachResolver, UserResolver, SkillResolver],
+    resolvers: [CoachResolver, UserResolver, SkillResolver, RequestResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
     globalMiddlewares: [TypegooseMiddleware],
     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
